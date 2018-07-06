fd100ee40762b54711cd3de36cf44a25f697fb52
Eugene Androsov <e.androsov@bpmonline.com>
2018-07-06 17:36:25 +0000

    Добавлена функция использования с примерами
    git-svn-id: http://ts1-def-svn02:8050/svn/InternalProject/trunk/Terrasoft.Tool.SVN@22 cbf3eb3d-e650-8a48-bf86-19b628dc3cc9

--- Terrasoft.Tools.SVN/Program.cs
+++ Terrasoft.Tools.SVN/Program.cs
@@ -4,10 +4,32 @@
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
+    using Microsoft.Win32;
 
     internal static class Program
     {
+        private const string TerrasoftToolsSvnExe = @"	Terrasoft.Tools.SVN.exe";
+        private const string OperationCreatefeature = " -Operation=CreateFeature";
+        private const string OperationUpdateFeature = " -Operation=UpdateFeature";
+        private const string OperationFinishFeature = " -Operation=FinishFeature";
+        private const string OperationCloseFeature = " -Operation=CloseFeature";
+        private const string SvnuserSvnuser = " -SvnUser=SvnUser";
+        private const string SvnpasswordSnvpassword = " -SvnPassword=SnvPassword";
+
+        private const string WorkingcopypathCSvnProjectTerrasoftFeature1 =
+            @" -WorkingCopyPath=C:\SVN\Project\Terrasoft_Feature1";
+
+        private const string BranchfeatureurlHttpSvnServerSvnProjectBranches =
+            " -BranchFeatureUrl=http://Svn-Server/svn/Project/branches";
+
+        private const string BranchreleaseurlHttpSvnServerSvnProjectTrunkPackagestore =
+            " -BranchReleaseUrl=http://Svn-Server/svn/Project/trunk/PackageStore";
+
+        private const string FeaturenameFeature1 = " -FeatureName=Feature1";
+        private const string CommitifnoerrorTrue = " -CommitIfNoError=true";
+
         private static readonly ConcurrentDictionary<string, string> _programOptions =
             new ConcurrentDictionary<string, string>();
 
@@ -15,12 +37,18 @@
             IEnumerable<string[]> argsEnumerable =
                 args.Select(argument => argument.Split('=')).Where(keyvalue => keyvalue.Length == 2);
             Parallel.ForEach(argsEnumerable, FillParamDelegate);
-            switch (_programOptions["operation"].ToLowerInvariant()) {
+            if (!_programOptions.ContainsKey("operation")) {
+                Usage();
+                return 0;
+            }
+
+            string programOption = _programOptions["operation"].ToLowerInvariant();
+            switch (programOption) {
                 case "createfeature":
                     using (var svnUtils = new SvnUtils(_programOptions)) {
                         return Convert.ToInt32(svnUtils.CreateFeature());
                     }
-                case "updatefeaturefromrelease":
+                case "updatefeature":
                     using (var svnUtils = new SvnUtils(_programOptions)) {
                         if (svnUtils.UpdateFromReleaseBranch() && Convert.ToBoolean(svnUtils.CommitIfNoError)) {
                             return Convert.ToInt32(svnUtils.CommitChanges(true));
@@ -38,6 +66,7 @@
                     using (var svnUtils = new SvnUtils(_programOptions)) {
                         svnUtils.DeleteClosedFeature();
                     }
+
                     break;
                 default:
                     Usage();
@@ -48,7 +77,75 @@
         }
 
         private static void Usage() {
-            Console.WriteLine("Usage:");
+            string language = Registry.GetValue(@"HKEY_CURRENT_USER\", @"", "Rus").ToString();
+            if (language == "Rus") {
+                Console.WriteLine("Использование:");
+                Console.WriteLine("\tTerrasoft.Tools.SVN.exe <команда> [аргументы]");
+                Console.WriteLine("Команды:");
+                Console.WriteLine(
+                    "\t-Operation:\t\tВ качестве операции выбрать одну из следующих операций: CreateFeature, UpdateFeature, FinishFeature, CloseFeature, ChangeLanguage");
+                Console.WriteLine("\t\tCreateFeature :\tВыделение новой ветки для фитчи на основании родительской");
+                Console.WriteLine("\t\tUpdateFeature :\tОбновить ветку фитчи изменениями из родительской ветки");
+                Console.WriteLine(
+                    "\t\tFinishFeature :\tЗавершить разработку фитчи, путем реинтеграции ветки фитчи в родительску ветку");
+                Console.WriteLine("\t\tCloseFeature  :\tПометь фитчу как завершенную, и закрыть ветку");
+                Console.WriteLine("\t\tChangeLanguage:\tИзменить язык уведомлений");
+                Console.WriteLine();
+                Console.WriteLine("\tАргументы:");
+                Console.WriteLine("\t\tSvnUser         :\tИмя пользователя SVN");
+                Console.WriteLine("\t\tSvnPassword     :\tПароль пользователя SVN");
+                Console.WriteLine("\t\tWorkingCopyPath :\tПуть к локальной рабочей копии");
+                Console.WriteLine(
+                    "\t\tCommitIfNoError :\tАвтоматически фиксировать изменения, в случае отсутсвия ошибок");
+                Console.WriteLine("\t\tBranchFeatureUrl:\tUrl в которой базируются все фитчи");
+                Console.WriteLine("\t\tBranchReleaseUrl:\tUrl в которой базируется релиз");
+                Console.WriteLine("\t\tMaintainer      :\tИздатель, фигурирует в названии ветки");
+                Console.WriteLine("\t\tFeatureName     :\tНазвание фитчи, фигурирует в названии ветки");
+                Console.WriteLine();
+                Console.WriteLine("\tПримеры использования:");
+                var sample1 = new StringBuilder();
+                sample1.Append(TerrasoftToolsSvnExe);
+                sample1.Append(OperationCreatefeature);
+                sample1.Append(SvnuserSvnuser);
+                sample1.Append(SvnpasswordSnvpassword);
+                sample1.Append(WorkingcopypathCSvnProjectTerrasoftFeature1);
+                sample1.Append(BranchfeatureurlHttpSvnServerSvnProjectBranches);
+                sample1.Append(BranchreleaseurlHttpSvnServerSvnProjectTrunkPackagestore);
+                sample1.Append(FeaturenameFeature1);
+                Console.WriteLine(sample1);
+                var sample2 = new StringBuilder();
+                sample2.Append(TerrasoftToolsSvnExe);
+                sample2.Append(OperationUpdateFeature);
+                sample2.Append(SvnuserSvnuser);
+                sample2.Append(SvnpasswordSnvpassword);
+                sample2.Append(WorkingcopypathCSvnProjectTerrasoftFeature1);
+                sample2.Append(CommitifnoerrorTrue);
+                Console.WriteLine();
+                Console.WriteLine(sample2);
+                
+                var sample3 = new StringBuilder();
+                sample3.Append(TerrasoftToolsSvnExe);
+                sample3.Append(OperationFinishFeature);
+                sample3.Append(SvnuserSvnuser);
+                sample3.Append(SvnpasswordSnvpassword);
+                sample3.Append(WorkingcopypathCSvnProjectTerrasoftFeature1);
+                Console.WriteLine();
+                Console.WriteLine(sample3);
+                
+                var sample4 = new StringBuilder();
+                sample4.Append(TerrasoftToolsSvnExe);
+                sample4.Append(OperationCloseFeature);
+                sample4.Append(SvnuserSvnuser);
+                sample4.Append(SvnpasswordSnvpassword);
+                sample4.Append(WorkingcopypathCSvnProjectTerrasoftFeature1);
+                Console.WriteLine();
+                Console.WriteLine(sample4);
+            } else {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("\tTerrasoft.Tools.SVN [options]");
+                Console.WriteLine("Options:");
+                Console.WriteLine("\t-Operation: select one of n");
+            }
         }
 
         private static void FillParamDelegate(string[] keyValueArray, ParallelLoopState arg2, long arg3) {
