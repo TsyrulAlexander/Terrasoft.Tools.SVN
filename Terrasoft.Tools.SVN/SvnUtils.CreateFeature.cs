a18475b2e6c07c6d8239f63ca58ce086cbc81ce6
Eugene Androsov <e.androsov@bpmonline.com>
2018-08-09 11:41:45 +0000

    #0 Исправление копирования фитчи Теперь при копировании указывается не последняя ревизия всего репозитория, а именно так с которой мы копируем
    git-svn-id: http://ts1-def-svn02:8050/svn/InternalProject/trunk/Terrasoft.Tool.SVN@27 cbf3eb3d-e650-8a48-bf86-19b628dc3cc9

--- Terrasoft.Tools.SVN/SvnUtils.CreateFeature.cs
+++ Terrasoft.Tools.SVN/SvnUtils.CreateFeature.cs
@@ -12,18 +12,22 @@
         /// </summary>
         /// <param name="featureName">Название фитчи</param>
         /// <param name="featureNewUrl">URL новой ветки фитчи</param>
+        /// <param name="revision">Номер ревизии из которой выделяется копия</param>
         /// <returns>Результат</returns>
-        private bool CopyBaseBranch(string featureName, string featureNewUrl) {
+        private bool CopyBaseBranch(string featureName, string featureNewUrl, long revision) {
             var svnCopyArgs = new SvnCopyArgs {
-                LogMessage = string.Format(Resources.SvnUtils_CopyBaseBranch_Init_Feature, featureName)
+                LogMessage = string.Format(Resources.SvnUtils_CopyBaseBranch_Init_Feature, featureName),
+                Revision = new SvnRevision(revision)
             };
             svnCopyArgs.Notify += SvnCopyArgsOnNotify;
             try {
                 return RemoteCopy(SvnTarget.FromString(BranchReleaseUrl), new Uri(featureNewUrl), svnCopyArgs);
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 Console.WriteLine(e);
                 return false;
-            } finally {
+            }
+            finally {
                 svnCopyArgs.Notify -= SvnCopyArgsOnNotify;
             }
         }
@@ -38,8 +42,9 @@
                 return false;
             }
 
+            long lastBranchRevision = GetBaseBranchHeadRevision(1, BranchReleaseUrl);
             string featureNewUrl = $"{BranchFeatureUrl}/{Maintainer}_{FeatureName}";
-            return CopyBaseBranch(FeatureName, featureNewUrl) &&
+            return CopyBaseBranch(FeatureName, featureNewUrl, lastBranchRevision) &&
                    Switch(workingCopyPath, SvnUriTarget.FromString(featureNewUrl));
         }
 
@@ -60,15 +65,17 @@
         /// <param name="workingCopyPath">Путь к рабочей копии</param>
         /// <returns>Результат</returns>
         private bool CheckouWorkingCopy(string workingCopyPath) {
-            var svnCheckOutArgs = new SvnCheckOutArgs() { IgnoreExternals = false };
+            var svnCheckOutArgs = new SvnCheckOutArgs() {IgnoreExternals = false};
             svnCheckOutArgs.Notify += SvnCheckOutArgsOnNotify;
             try {
                 string branchRelease = BranchReleaseUrl;
                 return CheckOut(SvnUriTarget.FromString(branchRelease), workingCopyPath, svnCheckOutArgs);
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 Console.WriteLine(e);
                 return false;
-            } finally {
+            }
+            finally {
                 svnCheckOutArgs.Notify -= SvnCheckOutArgsOnNotify;
             }
         }
@@ -79,17 +86,17 @@
         /// <param name="workingCopyPath">Путь к рабочей копии</param>
         /// <returns>Результат</returns>
         private bool UpdateWorkingCopy(string workingCopyPath) {
-            Info(SvnTarget.FromString(workingCopyPath), (sender, args) => {
-                Console.WriteLine(args);
-            });
-            var svnUpdateArgs = new SvnUpdateArgs { IgnoreExternals = false };
+            Info(SvnTarget.FromString(workingCopyPath), (sender, args) => { Console.WriteLine(args); });
+            var svnUpdateArgs = new SvnUpdateArgs {IgnoreExternals = false};
             svnUpdateArgs.Notify += SvnUpdateArgsOnNotify;
             try {
                 return Update(workingCopyPath, svnUpdateArgs);
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 Console.WriteLine(e);
                 return false;
-            } finally {
+            }
+            finally {
                 svnUpdateArgs.Notify -= SvnUpdateArgsOnNotify;
             }
         }
